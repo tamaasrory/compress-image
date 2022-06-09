a2e0a978782357a0d9785efb2b7add44bed95de7
tama <tama1234>
2022-06-10 02:56:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
+const fs = require("fs");
+
+const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+app.use(express.static("./uploads"));
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello world 🔥🇵🇹" });
+});
+
+app.post("/", upload.single("picture"), async (req, res) => {
+  fs.access("./uploads", (error) => {
+    if (error) {
+      fs.mkdirSync("./uploads");
+    }
+  });
+  const { buffer, originalname } = req.file;
+  const timestamp = new Date().toISOString();
+  const ref = `${timestamp}-${originalname}.png`;
+  await sharp(buffer)
+    .webp({ quality: 20 })
+    .toFile("./uploads/" + ref);
+  const link = `http://localhost:3000/${ref}`;
+  return res.json({ link });
+});
+
+app.listen(3000);
